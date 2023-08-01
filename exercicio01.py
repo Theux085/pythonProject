3d82af985d2bb4eab7e0763674f7f94ff6a1b9ab
ART0059292 <srmanigomes09@gmail.com>
2023-08-01 09:58:58 -0300

    antecessor e sucessor

---
+++ exercicio01.py
@@ -0,0 +1,10 @@
+# entrada de dados
+nota1 = float(input("Digite a primeira nota:  "))
+nota2 = float(input("Digite a segunda nota:  "))
+nota3 = float(input("Digite a terceira nota:  "))
+
+# Calculando a média
+media = (nota1 + nota2 + nota3) / 3
+
+# Exibindo a média
+print(f"A média das notas é: {media:.2f}")
